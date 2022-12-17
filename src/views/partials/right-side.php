5a23d6e48e0fda8a53c0c7bc30417c7f0c556efc
Gabriel Valmarath <melovalmarath.gabriel@gmail.com>
2022-12-16 23:07:03 -0300

    first commit

---
+++ src/views/partials/right-side.php
@@ -0,0 +1,18 @@
+<div class="box banners">
+    <div class="box-header">
+        <div class="box-header-text">Patrocinios</div>
+        <div class="box-header-buttons">
+            
+        </div>
+    </div>
+    <div class="box-body">
+        <a href=""><img src="https://alunos.b7web.com.br/media/courses/logo/php.jpg" /></a>
+        <a href=""><img src="https://alunos.b7web.com.br/media/courses/logo/laravel.jpg" /></a>
+    </div>
+</div>
+<div class="box">
+    <div class="box-body m-10">
+        Feito por: Gabriel Melo Valmarath
+        Para o curso de PHP da B7Web 
+    </div>
+</div>
